6f3d3f26a29796e329ff35a76b6d455ff4cfd275
Nikolaj J-K <kagenogmig@gmail.com>
2023-05-23 17:14:08 +0200

    Generate and randomize cards

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
+	var all_cards [162]int
+	for i := 0; i < 12*12; i++ {
+		all_cards[i] = i%12 + 1
+	}
+	for i := 12 * 12; i < 162; i++ {
+		all_cards[i] = -1
+	}
+
+	rand.Shuffle(len(all_cards), func(i, j int) {
+		all_cards[i], all_cards[j] = all_cards[j], all_cards[i]
+	})
+
 	fmt.Print("Hello World")
+	fmt.Print("{}", all_cards)
 }
