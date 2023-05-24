cf5865736752793c82c3d59dbdbf88d504fe199d
Nikolaj Jehøj-Krogager <kagenogmig@gmail.com>
2023-05-24 13:51:36 +0200

    implement stack

--- main.go
+++ main.go
@@ -5,6 +5,22 @@ import (
 	"math/rand"
 )
 
+type stack []int
+
+func (s *stack) push(e int) {
+	*s = append(*s, e)
+}
+
+func (s *stack) peek() int {
+	return (*s)[len(*s)-1]
+}
+
+func (s *stack) pop() int {
+	e := (*s)[len(*s)-1]
+	*s = (*s)[0 : len(*s)-1]
+	return e
+}
+
 func main() {
 	var all_cards [162]int
 	for i := 0; i < 12*12; i++ {
@@ -18,6 +34,13 @@ func main() {
 		all_cards[i], all_cards[j] = all_cards[j], all_cards[i]
 	})
 
-	fmt.Print("Hello World")
-	fmt.Print("{}", all_cards)
+	s1 := make(stack, 0)
+	s1.push(all_cards[10])
+	fmt.Println(s1)
+	fmt.Println(s1.pop())
+	fmt.Println(s1)
+
+	fmt.Println("Hello World")
+	fmt.Println(all_cards)
+
 }
